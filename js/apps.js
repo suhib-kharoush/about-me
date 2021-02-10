1987b93ecfcf9caf21371acfd3d730e2ae592b99
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-02-10 04:25:35 +0200

    loops edited

--- js/apps.js
+++ js/apps.js
@@ -88,16 +88,22 @@ else if (learn == 'no' || learn == 'n') {
     alert(message + ' you lost ' + score + ' points')
 }
 let foods = ['vegetables','fruits','meat','fish'];
-for (let i=5 ; i>1 ; i-- ){
-let prefer = prompt('guess what the type of food i prefer');
-while (prefer !=foods ) {prefer = prompt('guess what the type of food i prefer, you have '+ i +'attempts');
- prefer = parseint(prefer);
- break;} 
- if (prefer==foods) {y=y+1;
-    i=0 
- } else if (prefer>foods){alert('too high');} else {alert("too low");}
+for (let i=6 ; i>=1 ; i-- ){
+let prefer = prompt('guess what the type of food i prefer, you have '+i+' attempt');
+for (let x=0; x<4;x++)
+{if(prefer.toLowerCase()===foods[x]){
+a=a+1;
+i=0;
+}}
 }
-if (foods!=prefer){alert('my favourite food is 1')}
+// while (prefer !=foods ) {prefer = prompt('guess what the type of food i prefer, you have '+ i +'attempts');
+//  prefer = parseint(prefer);
+//  break;} 
+//  if (prefer==foods) {y=y+1;
+//     i=0 
+//  } else if (prefer>foods){alert('too high');} else {alert("too low");}
+// }
+// if (foods!=prefer){alert('my favourite food is 1')}
 // let field1 = 'mechanical'
 // let field2 = 'electrical'
 // let field3 = 'civil'
@@ -110,21 +116,21 @@ if (foods!=prefer){alert('my favourite food is 1')}
 // let field10 = 'renewable energy'
 
 
-let field = ['mechanical', 'electrical', 'civil', 'thermal', 'automotive', 'telecomunication', 'computer', 'power', 'biomedical', 'renewable energy'];
+// let field = ['mechanical', 'electrical', 'civil', 'thermal', 'automotive', 'telecomunication', 'computer', 'power', 'biomedical', 'renewable energy'];
 
 
-for (let i = 0; i < field.length; i++) {
-    let majorCheck = prompt(field[i]);
+// for (let i = 0; i < field.length; i++) {
+//     let majorCheck = prompt(field[i]);
 
-    if (majorCheck < 5) {
-        alert('too low');
+//     if (majorCheck < 5) {
+//         alert('too low');
 
-    } else if (majorCheck >= 5) { alert('too high') };
-    // field = [i];
-    if (field[i] == majorCheck) {
+//     } else if (majorCheck >= 5) { alert('too high') };
+//     // field = [i];
+//     if (field[i] == majorCheck) {
 
-    }
+//     }
 
-}
+// }
     // field = prompt(field);
 // field[1]
